2e838b860349b9951ca9825c6a00665293dcc6b4
Leonid221 <leonid.natochiy@gmail.com>
2023-01-27 22:56:42 +0200

    feat: add func brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = Object.fromEntries(bracketsConfig);
+  let keyStr = Object.keys(obj);
+  let result = [];
+  
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSimbol = str[i];
+
+    if (keyStr.includes(currentSimbol[i])) {
+      result.push(currentSimbol);
+    } else {
+      if (result.length === 0) {
+      return false;
+      }
+
+      let topElement = result[result.length - 1];
+
+      if (obj[currentSimbol] === topElement) {
+        result.pop();
+      } else {
+        return false;
+      }
+
+    }
+  }
+
+  return result.length === 0;
 }
